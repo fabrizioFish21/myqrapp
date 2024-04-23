852b9b2a42cbba5b4cbcaef7a903403f488fc9a8
fabrizioFish21 <167846066+fabrizioFish21@users.noreply.github.com>
2024-04-23 00:26:15 -0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+function generarQR(){
+
+
+    let input1 = document.getElementById('input1').value.trim();
+    let input2 = document.getElementById('input2').value.trim();
+    let input3 = document.getElementById('input3').value.trim();
+    let input4 = document.getElementById('input4').value.trim();
+
+
+    let textoConcatenado=input1+","+input2+","+input3+","+input4;
+
+
+   if(textoConcatenado===",,,"){
+    alert("Los datos estan vacios");
+   }
+
+
+   var qrCodeURL = 'https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=' + encodeURIComponent(textoConcatenado);
+
+
+   let qrImg=document.createElement('img');
+   qrImg.src=qrCodeURL;
+   qrImg.alt="CodigoQR";
+   
+   qrImg.classList.add('qr-code');
+
+
+   document.getElementById('qr-code').appendChild(qrImg);
+}
